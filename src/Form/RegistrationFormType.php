d79c03a512d293c8645dbc0b2f1104b64677911e
KWurZ-Ow <marius.proton@gmail.com>
2022-10-13 17:08:18 +0200

    validator on the email

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,9 +6,10 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -18,7 +19,13 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('username')
-            ->add('mail')
+            ->add('mail', TextType::class, [
+                'constraints' => [
+                    new Email([
+                        'message' => 'Please enter a mail',
+                    ])
+                ]
+            ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
@@ -35,8 +42,7 @@ class RegistrationFormType extends AbstractType
                         'max' => 4096,
                     ]),
                 ],
-            ])
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
