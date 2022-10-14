2a7f75f92ed0aa44a2919fc56a6984b61b312521
KWurZ-Ow <marius.proton@gmail.com>
2022-10-14 10:37:27 +0200

    translation

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -6,17 +6,20 @@ use App\Repository\SubjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class HomePageController extends AbstractController
 {
     #[Route('/', name: 'app_home_page')]
-    public function index(SubjectRepository $subjectRepository): Response
+    public function index(SubjectRepository $subjectRepository, TranslatorInterface $translator): Response
     {
         $subjects = $subjectRepository->findAll();
+        $title = $translator->trans('welcome');
 
         return $this->render('home_page/index.html.twig', [
             'controller_name' => 'HomePageController',
-            'subjects' => $subjects
+            'subjects' => $subjects,
+            'title' => $title
         ]);
     }
 }
