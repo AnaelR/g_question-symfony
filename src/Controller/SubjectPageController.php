8d182456539aea3fc59bf5cb31fddd0e6c7ca869
KWurZ-Ow <marius.proton@gmail.com>
2022-10-21 01:12:27 +0200

    CRUD

--- src/Controller/SubjectPageController.php
+++ src/Controller/SubjectPageController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Subject;
 use App\Entity\Comment;
 use App\Form\CommentFormType;
+use App\Form\SubjectType;
 use App\Repository\CommentRepository;
 use App\Repository\SubjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -52,5 +54,33 @@ class SubjectPageController extends AbstractController
         $newComment->setSubject($subjectRepository->find($id));
 
         return $newComment;
+    }
+
+    #[Route('/{id}', name: 'app_subject_delete', methods: ['POST'])]
+    public function delete(Request $request, Subject $subject, SubjectRepository $subjectRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$subject->getId(), $request->request->get('_token'))) {
+            $subjectRepository->remove($subject, true);
+        }
+
+        return $this->redirectToRoute('app_profil_page', [], Response::HTTP_SEE_OTHER);
+    }
+
+    #[Route('/{id}/edit', name: 'app_subject_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Subject $subject, SubjectRepository $subjectRepository): Response
+    {
+        $form = $this->createForm(SubjectType::class, $subject);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $subjectRepository->save($subject, true);
+
+            return $this->redirectToRoute('app_profil_page', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('subject/edit.html.twig', [
+            'subject' => $subject,
+            'form' => $form,
+        ]);
     }
 }
